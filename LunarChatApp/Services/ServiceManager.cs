0da623ca41ff3fe662f96c27e5e9af99771bae3f
Builderb <builderbgamer@gmail.com>
2025-12-04 03:16:52 +0000

    Fix server member and create event

--- LunarChatApp/Services/ServiceManager.cs
+++ LunarChatApp/Services/ServiceManager.cs
@@ -8,7 +8,7 @@ namespace LunarChatApp.Services;
 
 public sealed class ServiceManager
 {
-    public bool IsDev = false;
+    public bool IsDev = true;
     public readonly PageManager PageManager;
     public readonly TestState State;
     public readonly LunarRestClient Rest;
